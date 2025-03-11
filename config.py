89a5b9c2f15f2494303e61d634131e34e1b372ba
SHASHANK SHUKLA <shashankshukla69204@gmail.com>
2025-03-11 14:32:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -77,16 +77,16 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/job5jx.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/x9eu9k.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://files.catbox.moe/job5jx.jpg"
+    "PING_IMG_URL", "https://files.catbox.moe/x9eu9k.jpg"
 )
 PLAYLIST_IMG_URL = "https://telegra.ph/file/13afb9ee5c5da17930f1e.png"
-STATS_IMG_URL = "https://files.catbox.moe/job5jx.jpg"
-TELEGRAM_AUDIO_URL = "https://files.catbox.moe/job5jx.jpg"
-TELEGRAM_VIDEO_URL = "https://files.catbox.moe/job5jx.jpg"
-STREAM_IMG_URL = "https://files.catbox.moe/job5jx.jpg"
+STATS_IMG_URL = "https://files.catbox.moe/x9eu9k.jpg"
+TELEGRAM_AUDIO_URL = "https://files.catbox.moe/x9eu9k.jpg"
+TELEGRAM_VIDEO_URL = "https://files.catbox.moe/x9eu9k.jpg"
+STREAM_IMG_URL = "https://files.catbox.moe/x9eu9k.jpg"
 SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
 YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
